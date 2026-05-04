3c83e7ff5e89afdf16be938c6eb23881f987e419
Jeevantha Perera <jeevanthaa99@gmail.com>
2026-05-04 23:42:40 +0530

    implemented functionality of the nav bar using js

---
+++ script.js
@@ -0,0 +1,41 @@
+// Select all nav links
+const navLinks = document.querySelectorAll(".navLinks a");
+
+// Smooth scrolling
+navLinks.forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href");
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+
+// Active link on scroll
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("class");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href").includes(current)) {
+            link.classList.add("active");
+        }
+    });
+});
